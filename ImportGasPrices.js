316b9085109a1a6e90df73a646cf708dfc7f86b5
Nicholas Landers <nicholaslanders1234@gmail.com>
2023-09-23 23:06:57 -0300

    Made my budget spreadsheet do more of the work as that is my preference. So got rid of some fields, and added a prior gas price field for my reference.

--- ImportGasPrices.js
+++ ImportGasPrices.js
@@ -13,35 +13,33 @@ function main() {
     newVal.gasPrice = parseFloat(newVal.gasPrice);
     newVal.gasPrice = newVal.gasPrice.toFixed(1);
     
-    newVal.gasLastUpdated = "The last update was: " + $(".datetime").text();
-    newVal.timeStamp = Utilities.formatDate(new Date(), 'Etc/GMT', "yyyy-MM-dd HH:mm:ss");
+    // Off by an hour when daylight savings ends I believe
+    newVal.timeStamp = Utilities.formatDate(new Date(), 'GMT-3:00', "yyyy-MM-dd HH:mm:ss");
     
     return newVal;
   }
   
   // Very specific to the way I do my budget in google sheets 
   function updateSheet(newVal) {
-    let litresOfGas = 39;
-    let gasPrice = newVal.gasPrice /100;
+    let gasPrice = (newVal.gasPrice - 3.4) /100;
     let timeStamp = newVal.timeStamp;
-    let gasLastUpdated = newVal.gasLastUpdated;
-    let finalBudgetGas;
+    let priorGasPrice;
   
-    // To account for monthly gas, and also converting to proper number for dollars and cents
-    finalBudgetGas = ((gasPrice * litresOfGas) * 2).toFixed(2);
     // Removed personal sheet ID, which would go in the line below
     let budgetSheet = SpreadsheetApp.openById("");
     let sheet = budgetSheet.getSheets()[0];
     let finalBudgetCell = sheet.createTextFinder("Gas").findNext();
     finalBudgetCell = finalBudgetCell.offset(1, 0)
-    finalBudgetCell.setValue(finalBudgetGas);
+  
   
     let timeCell = finalBudgetCell.offset(1, 0);
     timeCell.setValue(timeStamp);
   
-    let lastUpdatedCell = finalBudgetCell.offset(2, 0);
-    lastUpdatedCell.setValue(gasLastUpdated);
   
     let gasCell = finalBudgetCell.offset(3, 0);
+    priorGasPrice = gasCell.getValue();
     gasCell.setValue(gasPrice);
+
+    let priorGasCell = finalBudgetCell.offset(3,1);
+    priorGasCell.setValue(priorGasPrice);
   }
